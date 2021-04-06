a9f8874c40a0f83c4aa303aeba5dab17f8f51ecf
igorovic <hello@dyve.dev>
2021-04-06 15:04:08 +0200

    v2

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-import QrScanner from 'qr-scanner';
+import QrScanner from "qr-scanner";
 
 const pre = document.getElementById("output");
-const videoElem = document.querySelector("video")
-const qrScanner = new QrScanner(videoElem, result => {
-  console.log('decoded qr code:', result);
-  pre.textContent = String(result);
-});
+const videoElem = document.querySelector("video");
+const qrScanner = new QrScanner(
+  videoElem,
+  (result) => {
+    console.log("decoded qr code:", result);
+    pre.textContent = String(result);
+  },
+  (err) => {
+    pre.textContent = String(err);
+  },
+  300,
+  "environment"
+);
 
-if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices) {
+qrScanner.start();
+/* if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices) {
   console.log("Let's get this party started");
   navigator.mediaDevices
     .getUserMedia({
@@ -20,8 +29,8 @@ if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices) {
     .then((stream) => {
       console.log("got media stream");
       pre.textContent = "got media stream";
-      
+
       videoElem.srcObject = stream;
       qrScanner.start();
     });
-}
+} */
